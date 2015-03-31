d1ff362ca6170b9695297356416b8daf5f513043
Adrien Cadet <acadet@live.fr>
2015-03-30 17:33:00 -0700

    Adds API

---
+++ app/src/main/java/org/railsschool/tiramisu/models/beans/SchoolClass.java
@@ -0,0 +1,11 @@
+package org.railsschool.tiramisu.models.beans;
+
+import java.util.List;
+
+/**
+ * @class SchoolClass
+ * @brief
+ */
+public class SchoolClass extends Lesson {
+    public List<User> students;
+}
---
+++ app/src/main/java/org/railsschool/tiramisu/remote/IRailsSchoolAPI.java
@@ -0,0 +1,31 @@
+package org.railsschool.tiramisu.remote;
+
+import org.railsschool.tiramisu.models.beans.Lesson;
+import org.railsschool.tiramisu.models.beans.SchoolClass;
+import org.railsschool.tiramisu.models.beans.User;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Path;
+
+/**
+ * @class IRailsSchoolAPI
+ * @brief
+ */
+public interface IRailsSchoolAPI {
+    public final static String LESSON_ROOT = "/lessons";
+    public final static String USER_ROOT   = "/users";
+
+    public final static String FORMAT = ".json";
+
+    @GET(LESSON_ROOT + FORMAT)
+    public void getLessons(Callback<List<Lesson>> callback);
+
+    @GET(LESSON_ROOT + "/{id}" + FORMAT)
+    public void getLesson(@Path("id") int id, Callback<SchoolClass> callback);
+
+    @GET(USER_ROOT + "/{id}" + FORMAT)
+    public void getUser(@Path("id") int id, Callback<User> callback);
+}

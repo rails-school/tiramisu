4f5a7a6ea338a74a312694ad2a50f41fcebd8409
Adrien Cadet <acadet@live.fr>
2015-03-30 17:58:22 -0700

    Creates outlet factory

---
+++ app/src/main/java/org/railsschool/tiramisu/views/LandingActivity.java
@@ -0,0 +1,16 @@
+package org.railsschool.tiramisu.views;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.railsschool.tiramisu.R;
+
+
+public class LandingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}

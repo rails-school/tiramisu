c062fedb6500e1117d7222ea2b224584d2b8ff56
Adrien Cadet <acadet@live.fr>
2015-03-30 17:22:57 -0700

    Creates beans

---
+++ app/src/main/java/org/railsschool/tiramisu/models/beans/Lesson.java
@@ -0,0 +1,14 @@
+package org.railsschool.tiramisu.models.beans;
+
+/**
+ * @class Lesson
+ * @brief
+ */
+public class Lesson {
+    public int    id;
+    public String title;
+    public String summary;
+    public String startTime;
+    public String endTime;
+    public int    teacherId;
+}

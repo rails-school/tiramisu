8f8aa5d824a4dcbc8200edcaa9ef1fe78baff68d
Adrien Cadet <acadet@live.fr>
2015-04-30 14:17:11 -0700

    Sends push notification when a new lesson is posted

--- app/src/main/java/org/railsschool/tiramisu/services/PubnubListenerService.java
+++ app/src/main/java/org/railsschool/tiramisu/services/PubnubListenerService.java
@@ -4,11 +4,19 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import com.pubnub.api.Callback;
 import com.pubnub.api.Pubnub;
+import com.pubnub.api.PubnubError;
 import com.pubnub.api.PubnubException;
 
 import org.railsschool.tiramisu.R;
+import org.railsschool.tiramisu.models.beans.Lesson;
+import org.railsschool.tiramisu.models.bll.serializers.LessonSerializer;
+import org.railsschool.tiramisu.utils.PushNotificationSystem;
+import org.railsschool.tiramisu.views.activities.MainActivity;
 
 /**
  * @class PubnubListenerService
@@ -31,12 +39,36 @@ public class PubnubListenerService extends IntentService {
 
         try {
             pubnub.subscribe(
-                "channel", new Callback() {
+                getString(R.string.pubnub_channel),
+                new Callback() {
                     @Override
                     public void successCallback(String channel, Object message) {
-                        Object o = message;
+                        Lesson lesson;
+                        Gson gson;
 
-                        Log.d("FOO", o.toString());
+                        gson = new GsonBuilder()
+                            .registerTypeAdapter(Lesson.class, new LessonSerializer())
+                            .create();
+
+                        lesson = gson.fromJson(
+                            message.toString(), new TypeToken<Lesson>() {
+                            }.getType()
+                        );
+
+                        new PushNotificationSystem(getApplicationContext())
+                            .notify(
+                                lesson.getTitle(),
+                                lesson.getSummary(),
+                                MainActivity.startOnClassDetails(
+                                    getApplicationContext(),
+                                    lesson.getSlug()
+                                )
+                            );
+                    }
+
+                    @Override
+                    public void errorCallback(String channel, PubnubError error) {
+                        Log.e(getClass().getSimpleName(), error.getErrorString());
                     }
                 }
             );
